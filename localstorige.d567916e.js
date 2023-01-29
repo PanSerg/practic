85fff464dc3ca30bee8faaaade3fbeb12e727cd3
PanSerg <108748956+PanSerg@users.noreply.github.com>
2023-01-29 15:26:16 +0000

    Deploying to gh-pages from @ PanSerg/practic@8abb4e2f10505d533ecb27fd3b315f05b1a8ea01 🚀

--- localstorige.d567916e.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("my-films",JSON.stringify({name:"Avatar",age:3})),console.log(localStorage);var savedFilms=localStorage.getItem("my-films");console.log("savedFilms",savedFilms);var parsedFilms=JSON.parse(savedFilms);console.log("parsedFilms",parsedFilms);
-//# sourceMappingURL=localstorige.d567916e.js.map
